553692c7493c4221e01e2b101c777396e235eb76
fengjijiao <16521693+fengjijiao@users.noreply.github.com>
2021-01-22 19:19:42 +0800

    Create compiler.sh

---
+++ compiler.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir build
+compiler() {
+  CGO_ENABLED=0 GOOS=$1 GOARCH=$2 go build -trimpath -o build/wpra-${1}-${2} main.go
+}
+compiler linux amd64
+compiler linux 386
+compiler linux arm64
+compiler linux arm
+compiler windows amd64
+compiler windows 386
+compiler darwin amd64
+#compiler darwin 386
